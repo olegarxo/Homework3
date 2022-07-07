90ce921d8856e402f8119cf0e9b17585303de949
oleg <olegarxo15@gmail.com>
2022-07-07 12:07:54 +0300

    Сделал третье задание

---
+++ homework_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int numb = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while(index <= numb)
+{
+    if (index <= (numb-1))
+    {
+    Console.Write($"{Math.Pow(index,3) }, ");
+    }
+    else
+    {
+        Console.Write($"{Math.Pow(index,3) }. ");
+    }
+    index++;
+}
